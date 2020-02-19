14019b010dcc671de9a45a58874742201260b6c3
YusufErgin <61090620+YusufErgin@users.noreply.github.com>
2020-02-19 19:26:36 +0300

    Delete baglanti.php

--- baglanti.php
+++
@@ -1,7 +0,0 @@
-<?php
-mysql_connect("localhost","root","");
-mysql_select_db("veri");
-mysql_query("set names utf8");
-mysql_query("set character set utf8");
-mysql_query("set collation_connection='utf8_turkish_ci'");
-?>
